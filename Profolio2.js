e02f0e11275605def456139248eb7558f3672e49
“Qwrld97” <Quangxle97@gmail.com>
2023-11-14 23:15:34 -0500

    im not an engineer what am i doing???

---
+++ Profolio2.js
@@ -0,0 +1,41 @@
+function updateNameFromJSON() {
+  fetch('form.json')
+    .then(response => response.json())
+    .then(data => {
+      //Project Name
+      const name = data.project2.name;
+      const nameElement = document.getElementById('projectName');
+      nameElement.textContent = `${name}`;
+
+      //Project Type
+      const projectType = data.project2.typeOfProject;
+      const projecttypeElement = document.getElementById('typeOfProject');
+      projecttypeElement.textContent = `${projectType}`;
+
+      //Project Description
+      const projectDescription = data.project2.description;
+      const projectDescriptionElement = document.getElementById('description');
+      projectDescriptionElement.textContent = `${projectDescription}`;
+
+      //skills
+      const skill = data.project2.skill;
+      //parse each skill into a list of skills
+      const skillList = skill.split(',');
+      //get the ul element
+      const skillElement = document.getElementById('skill');
+      //loop through the list of skills and add each one to the ul
+      for (let i = 0; i < skillList.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = skillList[i];
+        skillElement.appendChild(li);
+      }
+
+    })
+    .catch(error => {
+      console.error('Error fetching or parsing JSON:', error);
+    });
+}
+
+// Call the function as soon as the DOM content is loaded
+document.addEventListener('DOMContentLoaded', updateNameFromJSON);
+
